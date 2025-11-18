fe95be8328cd86c8489ce696055e56bc6c9c7662
BHANUPRATAP SINGH <bhanurjb21@gmail.com>
2025-11-18 12:39:42 -0800

    commit

--- Server/index.js
+++ Server/index.js
@@ -34,7 +34,7 @@ app.use("/api/taxpayment", taxPaymentRoutes);
 app.use("/api/reports", reportRoutes);
 app.use("/api/user", userRoutes);
 
-// Datbase connection
+// Datbase connectionn
 mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
